67b05f58161a214d308176e29a853aa1f1062d25
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-03-16 00:14:03 -0300

    feat(sidebar): indicar página actual en el sidebar

--- src/components/layout/Sidebar/SidebarLink.tsx
+++ src/components/layout/Sidebar/SidebarLink.tsx
@@ -2,18 +2,19 @@ import type { ReactNode } from 'preact/compat'
 import { Icon } from '../../ui/Icon'
 
 interface Props {
+  pathname?: string
   title: string
   href: string
   icon: () => ReactNode
 }
 
-export function SidebarLink ({ title, href, icon: LinkIcon }: Props) {
+export function SidebarLink ({ pathname, title, href, icon: LinkIcon }: Props) {
   return (
-    <a href={href} class='group h-10 w-full flex items-center gap-3 px-2.5 rounded-lg transition-colors text-neutral-400 hover:text-light hover:bg-accent'>
+    <a href={href} class={`${pathname === href ? 'active' : ''} group h-10 w-full flex items-center gap-3 px-2.5 rounded-lg transition-colors text-neutral-400 hover:text-light hover:bg-accent [.active]:text-primary [.active]:bg-primary/10 [.active]:hover:bg-primary/30`}>
       <Icon class='size-5 stroke-2'>
         <LinkIcon />
       </Icon>
-      <span class='text-sm'>{title}</span>
+      <span class='text-sm group-[.active]:font-semibold'>{title}</span>
     </a>
   )
 }
--- src/components/layout/Sidebar/SidebarLinkMini.tsx
+++ src/components/layout/Sidebar/SidebarLinkMini.tsx
@@ -2,17 +2,18 @@ import type { ReactNode } from 'preact/compat'
 import { Icon } from '../../ui/Icon'
 
 interface Props {
+  pathname?: string
   title: string
   href: string
   icon: () => ReactNode
 }
 
-export function SidebarLinkMini ({ title, href, icon: LinkIcon }: Props) {
+export function SidebarLinkMini ({ pathname, title, href, icon: LinkIcon }: Props) {
   return (
     <a
       href={href}
       title={title}
-      class='group size-10 flex items-center justify-center gap-3 rounded-lg transition-colors text-neutral-400 hover:text-light hover:bg-accent'
+      class={`${pathname === href ? 'active' : ''} group size-10 flex items-center justify-center gap-3 rounded-lg transition-colors text-neutral-400 hover:text-light hover:bg-accent [.active]:text-primary [.active]:bg-primary/10`}
     >
       <Icon class='size-5 stroke-2'>
         <LinkIcon />
