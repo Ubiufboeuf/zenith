48677e2536757abc9a35e27c50913e76e30fc487
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-03-16 23:08:23 -0300

    fix(ux): arreglar z-index del sidebar mini

--- src/components/layout/Sidebar/SidebarUserCardMini.tsx
+++ src/components/layout/Sidebar/SidebarUserCardMini.tsx
@@ -1,6 +1,6 @@
 export function SidebarUserCardMini () {
   return (
-    <footer class='h-full w-full flex items-center justify-center border-t border-border'>
+    <footer class='h-full w-full flex items-center justify-center border-t z-1 border-border'>
       <button
         title='Stelle (Trailblazer)'
         class='h-fit p-1 aspect-square flex items-center justify-center rounded-lg cursor-pointer transition-colors hover:bg-accent'
