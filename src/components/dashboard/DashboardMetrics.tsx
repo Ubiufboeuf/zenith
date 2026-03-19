4d0da35ca8771e7816d9d2f431499285c9b8baaf
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-03-19 18:12:47 -0300

    feat(dashboard): mejorar responsive de las métricas del dashboard

--- src/components/dashboard/DashboardMetrics.tsx
+++ src/components/dashboard/DashboardMetrics.tsx
@@ -11,21 +11,21 @@ const dashboardMetrics: MetricCardProps[] = [
 export function DashboardMetrics () {
   
   return (
-    <section class='h-fit w-full my-4 grid ml:grid-cols-2 lg:grid-cols-4 gap-2 sm:gap-4'>
+    <section class='h-fit w-full my-4 grid ml:grid-cols-2 lg:grid-cols-4 gap-2 ml:gap-4'>
       { dashboardMetrics.map(({ label, value, trend, icon: MetricIcon, color }) => (
         <article
           key={`metric-card-${label.toLowerCase().replaceAll(' ', '-')}`}
-          class='group grid grid-cols-[auto_1fr_auto] grid-rows-[1fr_auto] items-center justify-center gap-x-4 p-4 rounded-lg sm:rounded-xl border transition-colors bg-card border-border'
+          class='group grid grid-cols-[auto_1fr_auto] grid-rows-[1fr_auto] lg:grid-rows-[repeat(3,auto)] items-center justify-center gap-x-4 lg:gap-y-4 p-4 rounded-lg sm:rounded-xl border transition-colors bg-card border-border'
           style={{ '--color': color }}
         >
-          <div class='row-span-full size-10 flex items-center justify-center text-(--color) bg-(--color)/8 rounded-lg'>
+          <div class='row-span-full lg:row-1 size-10 flex items-center justify-center text-(--color) bg-(--color)/8 rounded-lg'>
             <Icon class='size-6 stroke-2'>
               <MetricIcon />
             </Icon>
           </div>
-          <h1 class='row-1 ml:sidebar-open:col-[2/4] text-xs text-neutral-400'>{label}</h1>
-          <strong class='text-m font-bold'>{value}</strong>
-          <div class='row-span-full ml:sidebar-open:row-2 ml:sidebar-open:col-3 flex gap-2 text-destructive'>
+          <h1 class='row-1 lg:row-2 lg:col-span-full ml:not-lg:sidebar-open:col-[2/4] text-xs text-neutral-400'>{label}</h1>
+          <strong class='lg:row-3 font-bold'>{value}</strong>
+          <div class='row-span-full lg:row-1 lg:-col-1 ml:not-lg:sidebar-open:row-2 ml:sidebar-open:col-3 flex justify-end gap-2 text-destructive'>
             <Icon class='size-5 stroke-2'>
               <IconArrow />
             </Icon>
