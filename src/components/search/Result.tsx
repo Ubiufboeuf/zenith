cf66cf13556c2f2b17de7e31b07c55c9ca4a8b5a
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-03-26 22:24:45 -0300

    feat(ui): mejorar visualmente los resultados en pantallas pequeñas

--- src/components/search/Result.tsx
+++ src/components/search/Result.tsx
@@ -9,20 +9,20 @@ export function Result ({ name, code, category, prices, priceId, stock }: Produc
 
   return (
     <article
-      class={`${enoughStock ? 's' : 'ns'} group h-20 w-full grid grid-cols-[auto_auto_auto_1fr_auto_auto] grid-rows-2 items-center p-4 px-4 gap-x-4 gap-y-2 rounded-xl border [.ns]:border-l-2 transition-colors border-border [.ns]:border-l-destructive bg-card shr:border-primary/40`}
+      class={`${enoughStock ? 's' : 'ns'} group h-fit xs:h-20 w-full grid grid-cols-[auto_2fr_auto] grid-rows-[44px_auto_44px] xs:grid-cols-[auto_auto_auto_1fr_auto_auto] xs:grid-rows-2 items-center p-4 px-4 gap-x-1 xs:gap-x-4 gap-y-2 rounded-xl border xs:[.ns]:border-l-2 transition-colors border-border xs:[.ns]:border-l-destructive bg-card shr:border-primary/40`}
     >
-      <div class='row-span-full size-11 flex items-center justify-center rounded-lg bg-border/70 group-[.ns]:bg-destructive/15'>
-        <Icon class='size-5s flex items-center justify-center stroke-2 text-neutral-500 group-[.ns]:text-destructive'>
+      <div class='not-xs:col-1 not-xs:row-1 xs:row-span-full size-9 xs:size-11 flex items-center justify-center rounded-lg bg-border/70 group-[.ns]:bg-destructive/15'>
+        <Icon class='size-5 xs:size-6 flex items-center justify-center stroke-2 text-neutral-500 group-[.ns]:text-destructive'>
           <IconBox />
         </Icon>
       </div>
-      <h1 title={name} class='col-[2/5] line-clamp-1 text-start font-medium text-light'>{capitalize(capitalize(name, '('), ' ')}</h1>
-      <span class='row-2 col-2 text-sm text-neutral-500'>{code}</span>
-      <span class='row-2 col-3 w-fit text-xs font-medium rounded p-0.25 px-1 border border-border text-light bg-dark'>{category}</span>
-      <strong class='row-span-full col-5 text-xl text-primary'>{price.currency} {price.value}</strong>
-      <div class='row-span-full col-6 flex flex-col items-end'>
+      <h1 title={name} class='col-span-full xs:col-[2/5] row-2 xs:row-1 line-clamp-2 text-start font-medium text-light'>{capitalize(capitalize(name, '('), ' ')}</h1>
+      <span class='row-1 xs:row-2 col-2 xs:col-2 text-sm not-xs:text-center text-neutral-400'>{code}</span>
+      <span class='row-1 xs:row-2 xs:col-3 w-fit text-xs not-xs:justify-self-end font-medium rounded p-0.25 px-1 border border-border text-light bg-dark'>{category}</span>
+      <strong class='row-3 xs:row-span-full col-3 xs:col-5 text-xl not-xs:justify-self-end text-primary'>{price.currency} {price.value}</strong>
+      <div class='row-3 xs:row-span-full col-span-2 xs:col-6 flex xs:flex-col items-center xs:items-end gap-x-2'>
         <span class='font-semibold group-[.ns]:text-destructive'>{stock.current}</span>
-        <span class='uppercase text-xs text-neutral-500'>{ enoughStock ? 'En stock' : `Min: ${stock.min}`}</span>
+        <span class='uppercase text-xs text-neutral-400'>{ enoughStock ? 'En stock' : `Min: ${stock.min}`}</span>
       </div>
     </article>
   )
