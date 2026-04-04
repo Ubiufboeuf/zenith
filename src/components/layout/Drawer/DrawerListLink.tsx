7c0761b3ad6f88e69bff198e9dc15b7aef2bcb75
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-04-03 22:34:07 -0300

    refactor(ui): cambiar color de texto con shr en el drawer

--- src/components/layout/Drawer/DrawerListLink.tsx
+++ src/components/layout/Drawer/DrawerListLink.tsx
@@ -5,7 +5,7 @@ export function DrawerListLink ({ title, href, details, color, icon: LinkIcon }:
   return (
     <a
       href={href}
-      class='w-full h-fit p-2 px-4 flex items-center col-span-2 gap-4 font-medium transition-colors text-light/70 shr:text-light/90 shr:bg-neutral-200/20'
+      class='w-full h-fit p-2 px-4 flex items-center col-span-2 gap-4 font-medium transition-colors text-light/70 shr:text-light shr:bg-neutral-200/20'
       style={{'--color': color}}
     >
       <div class='size-11 flex items-center justify-center rounded-xl bg-neutral-800/60'>
