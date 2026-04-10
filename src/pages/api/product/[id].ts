b09637a72e7d0aaaa1bd996188b68ac30985fc1f
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-04-09 22:50:18 -0300

    feat(api): crear endpoint para conseguir los datos de un producto

---
+++ src/pages/api/product/[id].ts
@@ -0,0 +1,51 @@
+import { db } from '@/database/db'
+import type { APIRoute } from 'astro'
+
+export async function GET ({ params }: Parameters<APIRoute>[0]) {
+  const id = params.id
+
+  console.log('[api] id', id)
+
+  if (!id) {
+    console.error('[api] Falta especificar un id')
+    
+    const error = {
+      msg: 'Falta especificar el id del producto'
+    }
+    
+    return new Response(JSON.stringify(error), {
+      headers: {
+        'content-type': 'application/json'
+      },
+      status: 400
+    })
+  }
+  
+  let product
+  try {
+    product = await db.getProductById(id)
+  } catch (err) {
+    console.error(`[api] Error consiguiendo el producto ${id}`)
+    console.error(err)
+
+    const error = {
+      msg: `Error consiguiendo el producto con id: ${id}`
+    }
+    
+    return new Response(JSON.stringify(error), {
+      headers: {
+        'content-type': 'application/json'
+      },
+      status: 404,
+      statusText: 'not_found'
+    })
+  }
+
+  console.log('[api] product:', product)
+
+  return new Response(JSON.stringify(product),  {
+    headers: {
+      'content-type': 'application/json'
+    }
+  })
+}
