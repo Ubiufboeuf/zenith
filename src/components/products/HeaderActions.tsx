dbdeabdf6c4ce366464ead6bd4aaa216a48b8502
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-04-12 22:13:51 -0300

    refactor(ui): cambios visuales a las acciones del header del producto
    sigo sin decidir bien el diseño

--- src/components/products/HeaderActions.tsx
+++ src/components/products/HeaderActions.tsx
@@ -15,13 +15,13 @@ export function HeaderActions () {
           <IconDownload />
         </Icon>
       </HeaderAction>
-      <HeaderAction title='Editar etiqueta' class='h-10 min-w-10 w-fit lg:px-3 gap-2 text-sm bg-neutral-800/70'>
+      <HeaderAction title='Editar etiqueta' class='h-10 min-w-10 w-fit lg:px-3 gap-2 text-sm border border-border bg-card! shr:bg-neutral-700/70!'>
         <Icon class='size-5 stroke-2'>
           <IconEdit />
         </Icon>
         <span class='not-lg:hidden'>Editar</span>
       </HeaderAction>
-      <HeaderAction title='Eliminar etiqueta' class='group h-10 min-w-10 w-fit lg:px-3 gap-2 text-sm text-destructive/80! group-shr:text-destructive! bg-destructive/20 shr:bg-destructive/40!'>
+      <HeaderAction title='Eliminar etiqueta' class='group h-10 min-w-10 w-fit lg:px-3 gap-2 text-sm border border-destructive/90 text-destructive/90! shr:bg-destructive/30!'>
         <Icon class='size-5 stroke-2'>
           <IconTrash />
         </Icon>
