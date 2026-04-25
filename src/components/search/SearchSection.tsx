990a3ee703e9461fad2200a3fecd7054e10891ce
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-04-24 21:15:13 -0300

    refactor: renombra la sección de búsqueda y simplifica nombres

--- src/components/search/SearchSection.tsx
+++ src/components/search/SearchSection.tsx
@@ -2,7 +2,7 @@ import { LayoutSwitcher } from './LayoutSwitcher'
 import { SearchBar } from './SearchBar'
 import { StockStatus } from './StockStatus'
 
-export function SearchBarSection () {
+export function SearchSection () {
   return (
     <section class='w-full h-fit grid grid-cols-[1fr_auto] grid-rows-[auto_1fr] gap-2 p-4 rounded-xl border border-border bg-card'>
       <SearchBar />
