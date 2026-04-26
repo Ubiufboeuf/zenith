e04db2b775f9e4e83a4a8f1379dbf65f8b599c95
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-04-26 20:23:12 -0300

    feat(ux): mejora detalles visuales y de experiencia en 'HeaderInfo'

--- src/components/product/HeaderInfo.tsx
+++ src/components/product/HeaderInfo.tsx
@@ -6,28 +6,30 @@ export function HeaderInfo () {
   const product = useProductStore((state) => state.product)
   if (!product) return
 
-  const { name, code, category } = product
+  const { name, code, category, stock } = product
+  const enoughStock = stock.current > stock.min
   
   return (
     <>
-      <h1 class='col-2 text-lg font-semibold text-light not-roomy-lg:hidden'>{name}</h1>
+      <h1 title={name} class='col-2 text-lg font-semibold line-clamp-1 text-light not-roomy-lg:hidden'>{name}</h1>
       <div class='col-2 w-full flex items-center gap-2 text-neutral-400 not-roomy-lg:row-span-full not-roomy-ml:hidden'>
         <span class='text-sm'>{code}</span>
         ·
         <span class='w-fit text-xs font-medium p-0.25 px-1 rounded-sm border border-border bg-dark'>{category}</span>
-        ·
-        <div class='flex items-center gap-1 text-xs text-orange-400'>
-          <Icon class='size-5'>
+        <div hidden={enoughStock} title='Stock bajo' class='flex items-center gap-1 text-xs'>
+          ·
+          <Icon class='size-5 text-orange-400'>
             <IconPackages />
           </Icon>
-          <span class='not-lg:hidden not-xl:sidebar-open:hidden'>Stock bajo</span>
+          <span class='not-roomy-xl:hidden text-orange-400'>Stock bajo</span>
         </div>
-        ·
-        <div class='flex items-center gap-1 text-xs text-orange-400'>
-          <Icon class='size-5'>
+        
+        <div title='Etiqueta desactualizada' class='flex items-center gap-1 text-xs'>
+          ·
+          <Icon class='size-5 text-orange-400'>
             <IconTag />
           </Icon>
-          <span class='not-lg:hidden not-xl:sidebar-open:hidden'>Etiqueta desactualizada</span>
+          <span class='not-roomy-xl:hidden text-orange-400'>Etiqueta desactualizada</span>
         </div>
       </div>
     </>
