66f300dcf0902b6a21fc5ea4e2ec179b085f6772
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-08-09 11:51:22 -0300

    fix(component): arregla problemas de espaciado del componente LineChart

--- src/components/ui/chart/LineChart.tsx
+++ src/components/ui/chart/LineChart.tsx
@@ -2,6 +2,10 @@
 import type { LineChartProps } from '@/types/ui/chartTypes'
 import { useId } from 'preact/hooks'
 
+const paddingX = 24
+const paddingY = 24
+const textSpace = 24
+
 export function LineChart ({ size, columns, rows, points, pointSize, pointStrokeWidth }: LineChartProps) {
   const id = useId()
   const gradientId = `gradient-${id}`
@@ -13,11 +17,20 @@ export function LineChart ({ size, columns, rows, points, pointSize, pointStroke
   const firstPoint = points[0]
   const lastPoint = points[points.length - 1]
 
-  const areaD = `${pathD} L ${lastPoint[0]} ${size[1]} L ${firstPoint[0]} ${size[1]} Z`
+  const [X, Y] = size
+
+  const viewBox = [
+    -paddingX,
+    -paddingY,
+    X + (paddingX * 2),
+    Y + paddingY + textSpace
+  ].join(' ')
+
+  const areaD = `${pathD} L ${lastPoint[0]} ${Y} L ${firstPoint[0]} ${Y} Z`
 
   return (
-    <article class='h-full w-full flex flex-col items-center p-8'>
-      <svg viewBox={`0 0 ${size.join(' ')}`} class='h-full w-[calc(100%-20px)] border-neutral-700 overflow-visible'>
+    <article class='h-full w-full flex flex-col items-center overflow-hidden'>
+      <svg viewBox={viewBox} class='h-full w-full border-neutral-700 overflow-hidden'>
         <defs>
           <linearGradient id={gradientId} x1='0' x2='0' y1='0' y2='1'>
             <stop offset='0%' stop-color='#6366f1' stop-opacity='0.35' />
@@ -26,14 +39,14 @@ export function LineChart ({ size, columns, rows, points, pointSize, pointStroke
         </defs>
         
         {/* Guías horizontales */}
-        { rows.map((r, idx, arr) => (
+        { rows.map((_, idx, arr) => (
           <line
-            key={`chart-${id}-line-${r || idx}`}
+            key={`chart-${id}-line-${idx}`}
             x1='0'
-            x2={size[0]}
-            y1={Math.round(size[1] / arr.length) * (idx) + 0.5}
-            y2={Math.round(size[1] / arr.length) * (idx) + 0.5}
-            stroke-dasharray='4 4'
+            x2={X}
+            y1={Math.round(Y / arr.length) * idx}
+            y2={Math.round(Y / arr.length) * idx}
+            stroke-dasharray='4'
             class='stroke-base-content/10'
           />
         )) }
@@ -51,15 +64,23 @@ export function LineChart ({ size, columns, rows, points, pointSize, pointStroke
           class='stroke-primary'
         />
 
-        {/* Puntos y columnas */}
-        { points.map(([x,y], idx) => <g key={`chart-${id}-point-${x}_${y}`}>
-          <circle
-            cx={x} cy={y} r={pointSize}
-            stroke-width={pointStrokeWidth}
-            class='fill-primary stroke-base-100'
-          />
-          <text x={x} y={size[1] + 16} text-anchor='middle' class='text-xs fill-base-content/50'>{columns[idx]}</text>
-        </g>) }
+        { points.map(([x, y], idx) => (
+          <g key={`chart-${id}-group-${idx}`}>
+            <circle
+              cx={x} cy={y} r={pointSize}
+              stroke-width={pointStrokeWidth}
+              class='fill-primary stroke-base-100'
+            />
+            <text
+              x={x}
+              y={Y + 16}
+              text-anchor='middle'
+              class='text-xs fill-base-content/50'
+            >
+              {columns[idx]}
+            </text>
+          </g>
+        )) }
       </svg>
     </article>
   )
