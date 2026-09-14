54043aa42e2bd8d4eb1ef6bacbbdaa32f6f7f74e
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-09-14 19:32:23 -0300

    feat: agrega constantes para los badges

---
+++ src/constants/ui/badgeConstants.ts
@@ -0,0 +1,12 @@
+import type { UIColors } from '@/types/uiTypes'
+
+export const BADGE_COLORS: Record<UIColors, string> = {
+  neutral: 'badge-neutral',
+  primary: 'badge-primary',
+  secondary: 'badge-secondary',
+  accent: 'badge-accent',
+  info: 'badge-info',
+  success: 'badge-success',
+  warning: 'badge-warning',
+  error: 'badge-error'
+}
