4d3d63c0ff9a87acba3c855d171c26f065a6bb92
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-09-21 23:41:24 -0300

    feat(ux): permite salir de los buscadores con esc

--- src/components/ui/search/toolbar/SearchSection.tsx
+++ src/components/ui/search/toolbar/SearchSection.tsx
@@ -4,9 +4,12 @@ import { IconSearch } from '../../Icons'
 import { Filters } from './Filters'
 import { SearchOptions } from './SearchOptions'
 import type { SearchSectionProps } from '@/types/ui/search/searchToolbarTypes'
+import { Keybinds } from '../../Keybinds'
+import { useRef } from 'preact/hooks'
 
 export function SearchSection ({ id, class: className = '', placeholder, onSearch }: SearchSectionProps) {
   const handleInput = (event: TargetedEvent<HTMLInputElement>) => onSearch(event.currentTarget.value)
+  const inputRef = useRef<HTMLInputElement>(null)
   
   return (
     <section class={`${className} w-full h-fit flex flex-row items-center gap-2 p-3 card border border-base-content/20 bg-base-100`}>
@@ -15,11 +18,13 @@ export function SearchSection ({ id, class: className = '', placeholder, onSearc
           <IconSearch />
         </Icon>
         <input
+          ref={inputRef}
           id={id}
           placeholder={placeholder}
           class='h-full w-full text-sm line-clamp-1 text-ellipsis'
           onInput={handleInput}
         />
+        <Keybinds keys='escape' onBind={() => inputRef.current?.blur()} hidden />
       </div>
       <Filters />
       <SearchOptions />
