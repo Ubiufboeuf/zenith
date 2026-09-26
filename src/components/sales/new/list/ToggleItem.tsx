81f66315912f389bac506193755ef96860de6cb6
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-09-26 07:04:40 -0300

    refactor(ux): agrega margen al ToggleItem de los productos listados

--- src/components/sales/new/list/ToggleItem.tsx
+++ src/components/sales/new/list/ToggleItem.tsx
@@ -4,10 +4,14 @@ export function ToggleItem () {
   const [checked] = useState(true)
 
   return (
-    <input
-      type='checkbox'
-      checked={checked}
-      class='checkbox checkbox-accent checkbox-sm'
-    />
+    <div class='w-full aspect-square p-2'>
+      <label class='flex items-center justify-center h-full w-full'>
+        <input
+          type='checkbox'
+          checked={checked}
+          class='checkbox checkbox-accent'
+        />
+      </label>
+    </div>
   )
 }
